c3f0682962a823f76216a495f7311ed874c649e8
Brian Deery <brian@factom.org>
2015-06-12 15:37:54 -0500

    reverted to allow merge

--- leveldb/table/writer.go
+++ leveldb/table/writer.go
@@ -12,12 +12,11 @@ import (
 	"fmt"
 	"io"
 
-	"code.google.com/p/snappy-go/snappy"
-
-	"github.com/btcsuitereleases/goleveldb/leveldb/comparer"
-	"github.com/btcsuitereleases/goleveldb/leveldb/filter"
-	"github.com/btcsuitereleases/goleveldb/leveldb/opt"
-	"github.com/btcsuitereleases/goleveldb/leveldb/util"
+	"github.com/btcsuite/goleveldb/leveldb/comparer"
+	"github.com/btcsuite/goleveldb/leveldb/filter"
+	"github.com/btcsuite/goleveldb/leveldb/opt"
+	"github.com/btcsuite/goleveldb/leveldb/util"
+	"github.com/btcsuite/snappy-go/snappy"
 )
 
 func sharedPrefixLen(a, b []byte) int {
